7dc3244074f8999b794b952c22da90e3f21c5232
Tom Zaworowski <tom@acidbits.com>
2019-05-21 21:47:16 -0700

    Add parser files

--- Sources/ArgumentParser/ArgumentParser.swift
+++ Sources/ArgumentParser/ArgumentParser.swift
@@ -1,3 +1,153 @@
-struct ArgumentParser {
-    var text = "Hello, World!"
+//
+//  ArgumentParser.swift
+//  ArgumentParser
+//
+//  Created by Tom Zaworowski on 5/21/19.
+//
+
+import Foundation
+
+public enum ParsingError: Error {
+    case missingParameterValue(Parameter)
+    case missingParameter(Parameter)
+    case missingOption
+    case missingVerb
+}
+
+public protocol Parsable {
+    typealias ResolvedParameter = (parameter: Parameter, value: String?)
+    typealias Callback = (_ parameters: [ResolvedParameter]?) -> Void
+    var name: String { get }
+    var callback: Callback? { get }
+    func test(arguments: [String]) throws -> Bool
+    func evaluate(arguments: [String]) throws
+}
+
+public extension Parsable {
+    func evaluate(arguments: [String]) throws {
+        if try test(arguments: arguments) {
+            callback?(nil)
+        }
+    }
+}
+
+public protocol Parser {
+    func parse(_ parsables: [Parsable]) throws
+}
+
+public struct Option: Parsable {
+    public let name: String
+    public let isRequired: Bool?
+    public let callback: Callback?
+    
+    public func test(arguments: [String]) throws -> Bool {
+        return arguments.contains(name) || isRequired == false
+    }
+}
+
+public struct Verb: Parsable {
+    public let name: String
+    public let parameters: [Parameter]?
+    public let callback: Callback?
+    
+    public func test(arguments: [String]) throws -> Bool {
+        var parameters = [ResolvedParameter]()
+        return try test(arguments: arguments, resolvedParameters: &parameters)
+    }
+    
+    public func evaluate(arguments: [String]) throws {
+        var parameters = [ResolvedParameter]()
+        if try test(arguments: arguments, resolvedParameters: &parameters) {
+            callback?(parameters)
+        }
+    }
+    
+    private func test(arguments: [String], resolvedParameters: inout [ResolvedParameter]) throws -> Bool {
+        guard arguments.first == name else {
+            return false
+        }
+        guard let parameters = parameters else {
+            return true
+        }
+        let arguments = Array(arguments.dropFirst())
+        for parameter in parameters {
+            guard let index = arguments.firstIndex(of: parameter.name) else {
+                if parameter.isRequired {
+                    throw ParsingError.missingParameter(parameter)
+                }
+                resolvedParameters.append((parameter, nil))
+                continue
+            }
+            let nextIndex = index.advanced(by: 1)
+            guard arguments.count > nextIndex else {
+                if parameter.valueRequired {
+                    throw ParsingError.missingParameterValue(parameter)
+                }
+                resolvedParameters.append((parameter, nil))
+                continue
+            }
+            let nextArgument = arguments[nextIndex]
+            guard parameters.contains(where: { $0.name == nextArgument }) == false else {
+                if parameter.valueRequired {
+                    throw ParsingError.missingParameterValue(parameter)
+                }
+                resolvedParameters.append((parameter, nil))
+                continue
+            }
+            resolvedParameters.append((parameter, nextArgument))
+        }
+        return true
+    }
+}
+
+public struct Group: Parsable {
+    enum ParsingError: Error {
+        case multipleVerbs
+    }
+    
+    public var name: String
+    public var verbs: [Verb]
+    public var callback: Callback?
+    
+    public func test(arguments: [String]) throws -> Bool {
+        var lastPass = false
+        for verb in verbs {
+            let pass = arguments.contains(verb.name)
+            if pass {
+                guard lastPass == false else {
+                    throw ParsingError.multipleVerbs
+                }
+                lastPass = pass
+            }
+        }
+        return lastPass
+    }
+    
+    public func evaluate(arguments: [String]) throws {
+        if try test(arguments: arguments) {
+            for verb in verbs {
+                try verb.evaluate(arguments: arguments)
+            }
+            callback?(nil)
+        }
+    }
+    
+}
+
+public struct Parameter: Hashable {
+    public let name: String
+    public let isRequired: Bool
+    public let valueRequired: Bool
+}
+
+extension CommandLine: Parser {
+    public static func parse(_ parsables: [Parsable]) throws {
+        for parsable in parsables {
+            try parsable.evaluate(arguments: Array(arguments.dropFirst()))
+        }
+    }
+    
+    public func parse(_ parsables: [Parsable]) throws {
+        try CommandLine.parse(parsables)
+    }
 }
--- Tests/ArgumentParserTests/ArgumentParserTests.swift
+++ Tests/ArgumentParserTests/ArgumentParserTests.swift
@@ -2,14 +2,112 @@ import XCTest
 @testable import ArgumentParser
 
 final class ArgumentParserTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ArgumentParser().text, "Hello, World!")
+    
+    override func setUp() {
+        CommandLine.arguments = [String(describing: self)]
     }
-
+    
+    func testSingleOption() {
+        let testOptionName = "-test"
+        CommandLine.arguments.append(testOptionName)
+        
+        let expectation = XCTestExpectation(description: "Verify callback")
+        let option = Option(name: testOptionName, isRequired: true) { (_) in
+            expectation.fulfill()
+        }
+        try! CommandLine.parse([option])
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testSingleVerb() {
+        let testVerbName = "test"
+        CommandLine.arguments.append(testVerbName)
+        
+        let expectation = XCTestExpectation(description: "Verify callback")
+        let verb = Verb(name: testVerbName, parameters: nil) { (parameters) in
+            expectation.fulfill()
+        }
+        try! CommandLine.parse([verb])
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testSingleVerbWithParameter() {
+        let testVerbName = "test"
+        let testParamName = "--source"
+        CommandLine.arguments.append(contentsOf: [testVerbName, testParamName])
+        
+        let expectation = XCTestExpectation(description: "Verify callback")
+        let parameter = Parameter(name: testParamName, isRequired: true, valueRequired: false)
+        let verb = Verb(name: testVerbName, parameters: [parameter]) { (parameters) in
+            XCTAssert(parameters?.count == 1, "Must have one parameter")
+            XCTAssert(parameters?.first?.parameter.name == testParamName, "Parameter must match")
+            expectation.fulfill()
+        }
+        try! CommandLine.parse([verb])
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testSingleVerbWithParameterValue() {
+        let testVerbName = "test"
+        let testParamName = "--source"
+        let testParamValue = "foo"
+        CommandLine.arguments.append(contentsOf: [testVerbName, testParamName, testParamValue])
+        
+        let expectation = XCTestExpectation(description: "Verify callback")
+        let parameter = Parameter(name: testParamName, isRequired: true, valueRequired: true)
+        let verb = Verb(name: testVerbName, parameters: [parameter]) { (parameters) in
+            XCTAssert(parameters?.count == 1, "Must have one parameter")
+            XCTAssert(parameters?.first?.parameter.name == testParamName, "Parameter must match")
+            XCTAssert(parameters?.first?.value == testParamValue, "Values must match")
+            expectation.fulfill()
+        }
+        try! CommandLine.parse([verb])
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testSingleVerbWithMultipleParametersValue() {
+        let testVerbName = "test"
+        let testParamName = "--source"
+        let testParamValue = "foo"
+        let secondParamName = "--output"
+        CommandLine.arguments.append(contentsOf: [testVerbName, testParamName, testParamValue])
+        
+        let expectation = XCTestExpectation(description: "Verify callback")
+        let parameter = Parameter(name: testParamName, isRequired: true, valueRequired: true)
+        let secondParameter = Parameter(name: secondParamName, isRequired: false, valueRequired: false)
+        let verb = Verb(name: testVerbName, parameters: [parameter, secondParameter]) { (parameters) in
+            XCTAssert(parameters?.count == 2, "Must have two parameters")
+            XCTAssert(parameters?.first?.parameter.name == testParamName, "Parameter must match")
+            XCTAssert(parameters?.first?.value == testParamValue, "Values must match")
+            XCTAssert(parameters?.last?.parameter.name == secondParamName, "Parameter must much")
+            expectation.fulfill()
+        }
+        try! CommandLine.parse([verb])
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testVerbGroup() {
+        let firstTestVerbName = "test1"
+        let secondTestVerbName = "test2"
+        CommandLine.arguments.append(contentsOf: [firstTestVerbName])
+        
+        let expectation = XCTestExpectation(description: "Verify callback")
+        let firstVerb = Verb(name: firstTestVerbName, parameters: nil) { (parameters) in
+            expectation.fulfill()
+        }
+        let secondVerb = Verb(name: secondTestVerbName, parameters: nil) { (parameters) in
+            expectation.fulfill()
+        }
+        let group = Group(name: "verbs", verbs: [firstVerb, secondVerb], callback: nil)
+        try! CommandLine.parse([group])
+        wait(for: [expectation], timeout: 1)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testSingleOption", testSingleOption),
+        ("testSingleVerb", testSingleVerb),
+        ("testSingleVerbWithParameterValue", testSingleVerbWithParameterValue),
+        ("testSingleVerbWithMultipleParametersValue", testSingleVerbWithMultipleParametersValue),
+        ("testVerbGroup", testVerbGroup),
     ]
 }
